8308d103048081e9e1d53c36bc48bc302a6174ec
sskyy <skyking_H@hotmail.com>
2025-02-22 23:08:05 +0800

    fix: update axii-ui

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -11,12 +11,12 @@ export default {
   },
   resolve: {
     alias: {
-      'axii-ui-theme-common': fileURLToPath(new URL('../theme-common/src/index.ts', import.meta.url)),
-      'axii-ui-theme-inc': fileURLToPath(new URL('../theme-inc/src', import.meta.url)),
-      'axii-ui-theme-fallout': fileURLToPath(new URL('../theme-fallout/src', import.meta.url)),
-      'axii-ui': fileURLToPath(new URL('../components/src/index.ts', import.meta.url)),
-      'axii': fileURLToPath(new URL('../../../axii/src/index.ts', import.meta.url)),
-      'data0': fileURLToPath(new URL('../../../data0/src/index.ts', import.meta.url))
+      // 'axii-ui-theme-common': fileURLToPath(new URL('../theme-common/src/index.ts', import.meta.url)),
+      // 'axii-ui-theme-inc': fileURLToPath(new URL('../theme-inc/src', import.meta.url)),
+      // 'axii-ui-theme-fallout': fileURLToPath(new URL('../theme-fallout/src', import.meta.url)),
+      // 'axii-ui': fileURLToPath(new URL('../components/src/index.ts', import.meta.url)),
+      // 'axii': fileURLToPath(new URL('../../../axii/src/index.ts', import.meta.url)),
+      // 'data0': fileURLToPath(new URL('../../../data0/src/index.ts', import.meta.url))
     }
   },
   server: {
